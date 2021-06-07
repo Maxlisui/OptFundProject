90e865d3bdd8bff8892d955eb13f53aa8bc5c496
Daniel Eberharter <daniel.eberharter@student.uibk.ac.at>
2021-06-07 12:26:14 +0200

    implement firefly algorithm

--- src/main/java/at/uibk/dps/optfund/dtlz/Main.java
+++ src/main/java/at/uibk/dps/optfund/dtlz/Main.java
@@ -1,6 +1,6 @@
 package at.uibk.dps.optfund.dtlz;
 
-import at.uibk.dps.optfund.dtlz.optimizer.FireflyOptimizerModule;
+import at.uibk.dps.optfund.dtlz.optimizer.FireflyAlgorithmModule;
 import org.opt4j.benchmarks.dtlz.DTLZModule;
 import org.opt4j.core.Individual;
 import org.opt4j.core.optimizer.Archive;
@@ -10,21 +10,29 @@ import org.opt4j.viewer.ViewerModule;
 public class Main {
 
     public static void main(String[] args) {
-        FireflyOptimizerModule ea = new FireflyOptimizerModule();
-        ea.setIterations(500);
-        ea.setMutationRate(0.1);
+        FireflyAlgorithmModule ea = new FireflyAlgorithmModule();
+        ea.setIterations(10000);
+        ea.setAttractivenessCoefficient(1.0);
+        ea.setLightAbsorptionCoefficient(0.05);
+        ea.setRandomWalkCoefficient(0.01);
+        ea.setNumberOfFireflies(100);
+
         DTLZModule dtlz = new DTLZModule();
         dtlz.setFunction(DTLZModule.Function.DTLZ1);
+        dtlz.setAlpha(100);
+        dtlz.setM(1);
+        dtlz.setN(1000);
+        dtlz.setK(1000);
+        dtlz.setEncoding(DTLZModule.Encoding.DOUBLE);
+        dtlz.setBits(30);
+
         ViewerModule viewer = new ViewerModule();
         viewer.setCloseOnStop(false);
+
         Opt4JTask task = new Opt4JTask(false);
         task.init(ea,dtlz,viewer);
         try {
             task.execute();
-            Archive archive = task.getInstance(Archive.class);
-            for (Individual individual : archive) {
-                // obtain the phenotype and objective, etc. of each individual
-            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
---
+++ src/main/java/at/uibk/dps/optfund/dtlz/optimizer/FireflyAlgorithmModule.java
@@ -0,0 +1,101 @@
+package at.uibk.dps.optfund.dtlz.optimizer;
+
+import org.opt4j.core.config.annotations.Info;
+import org.opt4j.core.optimizer.MaxIterations;
+import org.opt4j.core.optimizer.OptimizerModule;
+import org.opt4j.core.start.Constant;
+
+/**
+ * Module for firefly optimization
+ *
+ * @author Daniel Eberharter
+ */
+public class FireflyAlgorithmModule extends OptimizerModule {
+
+    @MaxIterations
+    protected int iterations = 1000;
+
+    @Constant(value = "numberOfFireflies", namespace = FireflyAlgorithm.class)
+    @Info("The number of fireflies.")
+    protected int numberOfFireflies = 100;
+
+    @Constant(value = "randomWalkCoefficient", namespace = FireflyAlgorithm.class)
+    @Info("The random walk parameter. (alpha)")
+    protected double randomWalkCoefficient = 1;
+
+    @Constant(value = "attractivenessCoefficient", namespace = FireflyAlgorithm.class)
+    @Info("The attractiveness (weight) parameter. (beta)")
+    protected double attractivenessCoefficient = 1;
+
+    @Constant(value = "lightAbsorptionCoefficient", namespace = FireflyAlgorithm.class)
+    @Info("The light absorption coefficient. (delta)")
+    protected double lightAbsorptionCoefficient = 0.5;
+
+    /**
+     * @return the number of iterations
+     */
+    public int getIterations() {
+        return iterations;
+    }
+
+    /**
+     * @param iterations the number of iterations
+     */
+    public void setIterations(int iterations) {
+        this.iterations = iterations;
+    }
+
+    /**
+     * @return the number of fireflies
+     */
+    public int getNumberOfFireflies() {
+        return numberOfFireflies;
+    }
+
+    /**
+     * @param numberOfFireflies the number of fireflies
+     */
+    public void setNumberOfFireflies(int numberOfFireflies) {
+        this.numberOfFireflies = numberOfFireflies;
+    }
+
+    /**
+     * @return the attractiveness coefficient
+     */
+    public double getAttractivenessCoefficient() {
+        return attractivenessCoefficient;
+    }
+
+    /**
+     * @param attractivenessCoefficient the attractiveness coefficient
+     */
+    public void setAttractivenessCoefficient(double attractivenessCoefficient) { this.attractivenessCoefficient = attractivenessCoefficient; }
+
+    /**
+     * @return the random walk coefficient
+     */
+    public double getRandomWalkCoefficient() { return randomWalkCoefficient; }
+
+    /**
+     * @param randomWalkCoefficient the random walk coefficient
+     */
+    public void setRandomWalkCoefficient(double randomWalkCoefficient) { this.randomWalkCoefficient = randomWalkCoefficient; }
+
+    /**
+     * @return the light absorption coefficient
+     */
+    public double getLightAbsorptionCoefficient() { return lightAbsorptionCoefficient; }
+
+    /**
+     * @param lightAbsorptionCoefficient the light absorption coefficient
+     */
+    public void setLightAbsorptionCoefficient(double lightAbsorptionCoefficient) { this.lightAbsorptionCoefficient = lightAbsorptionCoefficient; }
+
+    /**
+     * bind concrete implementations for dependency injection
+     */
+    @Override
+    protected void config() {
+        bindIterativeOptimizer(FireflyAlgorithm.class);
+    }
+}
