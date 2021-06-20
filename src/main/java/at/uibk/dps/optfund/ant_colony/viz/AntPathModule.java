2a950e8cb832ad78a7e8d9b7922dff64626833a5
Christoph Haas <christoph.h@sprinternet.at>
2021-06-20 23:00:59 +0200

    WIP: ant path visualization

--- src/main/java/at/uibk/dps/optfund/ant_colony/viz/AntPathModule.java
+++ src/main/java/at/uibk/dps/optfund/ant_colony/viz/AntPathModule.java
@@ -1,11 +1,32 @@
 package at.uibk.dps.optfund.ant_colony.viz;
 
+import org.opt4j.core.config.annotations.Info;
+import org.opt4j.core.config.annotations.Order;
 import org.opt4j.viewer.VisualizationModule;
 
+/**
+ * The implementation of the ant visualisation module.
+ * @author Christoph Haas
+ */
 public class AntPathModule extends VisualizationModule {
+
+    @Info("Visualize on optimization start.")
+    @Order(0)
+    protected boolean vizOnStart = true;
+
+    public boolean getVizOnStart() {
+        return vizOnStart;
+    }
+
+    public void setVizOnStart(boolean vizOnStart) {
+        this.vizOnStart = vizOnStart;
+    }
 
     @Override
     protected void config() {
+        bind(boolean.class).toInstance(vizOnStart);
         addIndividualMouseListener(AntPathService.class);
+        addOptimizerIterationListener(AntPathService.class);
+        addOptimizerStateListener(AntPathService.class);
     }
 }
--- src/main/java/at/uibk/dps/optfund/ant_colony/viz/AntPathService.java
+++ src/main/java/at/uibk/dps/optfund/ant_colony/viz/AntPathService.java
@@ -1,7 +1,14 @@
 package at.uibk.dps.optfund.ant_colony.viz;
 
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import org.opt4j.core.Individual;
+import org.opt4j.core.optimizer.Archive;
+import org.opt4j.core.optimizer.Optimizer;
+import org.opt4j.core.optimizer.OptimizerIterationListener;
+import org.opt4j.core.optimizer.OptimizerStateListener;
+import org.opt4j.tutorial.salesman.SalesmanProblem;
+import org.opt4j.tutorial.salesman.SalesmanRoute;
 import org.opt4j.viewer.IndividualMouseListener;
 import org.opt4j.viewer.Viewport;
 import org.opt4j.viewer.Widget;
@@ -10,15 +17,38 @@ import org.opt4j.viewer.WidgetParameters;
 import javax.swing.*;
 import java.awt.*;
 
-public class AntPathService implements IndividualMouseListener {
+/**
+ * The main logic for the visualization panels.
+ * @author Christoph Haas
+ */
+@Singleton
+public class AntPathService implements IndividualMouseListener, OptimizerIterationListener, OptimizerStateListener {
     protected final Viewport viewport;
+    protected AntPathWidget widget;
+    protected final boolean vizOnStart;
+    protected final Archive archive;
+
 
     // The path is shown by a double click of a individual in the archive
     // monitor panel. Thus we need the ArchiveMonitorPanel and the main
     // GUIFrame.
     @Inject
-    public AntPathService(Viewport viewport) {
+    public AntPathService(Viewport viewport, boolean vizOnStart, Archive archive) {
         this.viewport = viewport;
+        this.vizOnStart = vizOnStart;
+        this.archive = archive;
+    }
+
+    // Triggers if one full iteration completed.
+    @Override
+    public void iterationComplete(int iteration) {
+        if(widget == null) {
+            return; // no widget, nothing to do
+        }
+        if(widget.getIndividual() == null && vizOnStart && archive.stream().findFirst().isPresent()) {
+            widget.setIndividual(archive.stream().findFirst().get());
+        }
+        widget.getPanel().repaint();
     }
 
     // If an individual is clicked with the right mouse button, open a popup
@@ -39,55 +69,108 @@ public class AntPathService implements IndividualMouseListener {
     // Paint the path: Construct a JInternalFrame, add the AntPathPanel and add the
     // frame to the desktop of the main GUIFrame.
     private void paintPath(Individual individual) {
-        viewport.addWidget(new AntPathWidget(individual));
+        if(widget == null) {
+            widget = new AntPathWidget(individual);
+            viewport.addWidget(widget);
+        }
     }
 
-    // Panel that paints a single Ant Path
-    private static class AntPathPanel extends JPanel {
-        private static final long serialVersionUID = 1L;
-
-        protected final Individual individual;
-
-        public AntPathPanel(Individual individual) {
-            super();
-            this.individual = individual;
-            setPreferredSize(new Dimension(208, 208));
+    @Override
+    public void optimizationStarted(Optimizer optimizer) {
+        if(vizOnStart) {
+            paintPath(null);
         }
+    }
 
-        @Override
-        protected void paintComponent(Graphics g) {
-            Graphics2D g2d = (Graphics2D) g;
-
-            Rectangle size = g2d.getClip().getBounds();
-
-            g2d.setBackground(Color.WHITE);
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2d.setStroke(new BasicStroke(2f));
-            g2d.clearRect(0, 0, size.width, size.height);
-
-            g2d.fillOval(10,10,10,10);
-
-            final double factorX = size.width / 100.0;
-            final double factorY = size.height / 100.0;
-        }
+    @Override
+    public void optimizationStopped(Optimizer optimizer) {
     }
 
     @WidgetParameters(title = "Route")
     protected static class AntPathWidget implements Widget {
-        final Individual individual;
+        Individual individual;
+        AntPathPanel pathPanel;
 
         public AntPathWidget(Individual individual) {
             super();
             this.individual = individual;
         }
 
+        public Individual getIndividual() {
+            return individual;
+        }
+
+        public void setIndividual(Individual individual) {
+            this.individual = individual;
+            pathPanel.setIndividual(individual);
+        }
+
         @Override
         public JPanel getPanel() {
-            return new AntPathPanel(individual);
+            if(pathPanel == null) {
+                pathPanel = new AntPathPanel(individual);
+            }
+            return pathPanel;
         }
 
         @Override
         public void init(Viewport viewport) { }
 
+    }
+
+    // Panel that paints a single Ant Path
+    private static class AntPathPanel extends JPanel {
+        private static final long serialVersionUID = 1L;
+
+        protected Individual individual;
+
+        public AntPathPanel(Individual individual) {
+            super();
+            this.individual = individual;
+            setPreferredSize(new Dimension(416, 416));
+        }
+
+        public Individual getIndividual() {
+            return individual;
+        }
+
+        public void setIndividual(Individual individual) {
+            this.individual = individual;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            if(individual == null) {
+                return; // nothing to do
+            }
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setBackground(Color.WHITE);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setStroke(new BasicStroke(2f));
+            g2d.clearRect(0, 0, 416, 424);
+
+            SalesmanRoute salesmanRoute = (SalesmanRoute) individual.getPhenotype();
+
+            for (int i = 0; i < salesmanRoute.size(); i++) {
+                final int j = (i + 1) % salesmanRoute.size();
+                SalesmanProblem.City one = salesmanRoute.get(i);
+                SalesmanProblem.City two = salesmanRoute.get(j);
+
+                int x1 = (int) (one.getX() * 4) + 8;
+                int y1 = (int) (one.getY() * 4) + 8;
+                int x2 = (int) (two.getX() * 4) + 8;
+                int y2 = (int) (two.getY() * 4) + 8;
+
+                g2d.drawLine(x1, y1, x2, y2);
+
+                if(i == 0 || i == salesmanRoute.size() - 1)  { // the start/end point
+                    g2d.setColor(Color.RED);
+                }
+                g2d.fillOval(x1 - 2, y1 - 2, 8, 8);
+                if(i == 0 || i == salesmanRoute.size() - 1)  {
+                    g2d.setColor(Color.BLACK);
+                }
+            }
+        }
     }
 }
